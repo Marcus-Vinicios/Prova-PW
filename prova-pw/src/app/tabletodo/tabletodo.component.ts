ebde4eeee9e9e9443a950ef0552a75c11df617e4
httpsGabrielle <gabrielleso125@gmail.com>
2022-12-13 10:12:49 -0300

    primeiro commit

---
+++ prova-pw/src/app/tabletodo/tabletodo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabletodo',
+  template: `
+    <p>
+      tabletodo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TabletodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
