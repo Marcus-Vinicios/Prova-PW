400e968470c4d3d93a479351c57c03a96fdd5bdf
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2022-12-13 10:33:13 -0300

    primeiro commit

--- prova/README.md
+++ prova/README.md
@@ -1,4 +1,4 @@
-# ProvaPw
+# Prova
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
--- prova/src/app/app.module.ts
+++ prova/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TabletodoComponent } from './tabletodo/tabletodo.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabletodoComponent
+    TodoComponent
   ],
   imports: [
     BrowserModule,
